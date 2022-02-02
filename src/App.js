126d5890fcdd88ef94a020f09a0754ebe6ef540b
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2022-02-02 15:29:24 +0000

    File and Folder structure updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 // import { Article, Brand, CTA } from "./components";
 import { Footer, Blog, Possibility, WhatGPT3, Features, Header } from "./containers";
-import {CTA, Brand, Navbar} from "./components";
+import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
-  return( 
-  <div className="app">
-     <div className="gradient_bg">
-       <Navbar/>
-       <Header/>
-     </div>
-     <Brand/>
-     <WhatGPT3/>
-     <Features/>
-     <Possibility/>
-     <CTA/>
-      <Blog/>
+  return (
+    <div className="app">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
 
-  </div> 
+    </div>
   );
 };
 
